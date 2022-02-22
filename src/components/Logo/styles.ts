dd51bfc5be37dcbc6152bc89c950b40f37429a5a
carlosaLW <carlosa@lonewolfcorp.com>
2022-02-22 13:06:41 -0300

    feat: add new logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import media from "styled-media-query";
 
 export const LogoWrapper = styled.img`
-  width: 15rem;
+  width: 20rem;
   cursor: pointer;
 
   ${media.lessThan("medium")`
