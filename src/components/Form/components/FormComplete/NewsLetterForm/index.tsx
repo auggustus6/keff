f5302624ed6d76fcf0dcab198702cf0d63b1285b
Braiant Malta <braiant@lonewolrcorp.com>
2022-03-08 17:49:18 -0300

    build: removing divs and restore new form layout

--- src/components/Form/components/FormComplete/NewsLetterForm/index.tsx
+++ src/components/Form/components/FormComplete/NewsLetterForm/index.tsx
@@ -1,6 +1,8 @@
-import { FormDataProps } from "components/Form/types";
-import router from "next/router";
 import { useState } from "react";
+import router from "next/router";
+import { FormDataProps } from "components/Form/types";
+import * as Styles from "./styles";
+import InputMask from "react-input-mask";
 
 type NewsLetterFormType = {
   status: any;
@@ -13,11 +15,6 @@ export const NewsLetterForm = ({
   // message,
   onValidated,
 }: NewsLetterFormType) => {
-  // MailChimp States
-  // const [fName, setFName] = useState<string | undefined>("");
-  // const [email, setEmail] = useState<string | undefined>("");
-  // const [phone, setPhone] = useState<string | undefined>("");
-  // Plain States
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<FormDataProps>({
@@ -37,9 +34,9 @@ export const NewsLetterForm = ({
   };
   const { name, email, phone, visited } = data;
 
-  const handleChangeData = (data: FormDataProps) => {
-    setData(data);
-  };
+  // const handleChangeData = (data: FormDataProps) => {
+  //   setData(data);
+  // };
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -54,15 +51,6 @@ export const NewsLetterForm = ({
         EMAIL: email,
         PHONE: phone,
       });
-    // fName &&
-    //   email &&
-    //   phone &&
-    //   email.indexOf("@") > -1 &&
-    //   onValidated({
-    //     FNAME: fName,
-    //     EMAIL: email,
-    //     PHONE: phone,
-    //   });
 
     const form = new FormData();
     form.append("Nome", name);
@@ -89,114 +77,138 @@ export const NewsLetterForm = ({
     }
   };
 
-  // const getMessage = (message) => {
-  //   if (!message) {
-  //     return null;
-  //   }
-  //   const result = message?.split("-") ?? null;
-  //   if ("0" !== result?.[0]?.trim()) {
-  //     return decode(message);
-  //   }
-  //   const formattedMessage = result?.[1]?.trim() ?? null;
-  //   return formattedMessage ? decode(formattedMessage) : null;
-  // };
-
   return (
-    <div id="mc_embed_signup">
+    <Styles.Container>
+      {/* <div id="mc_embed_signup"> */}
+      <h4>Agora você pode criar sua própria marca!</h4>
       <form id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form">
-        <div id="mc_embed_signup_scroll">
-          <h2>Preencha abaixo para ser atendido!</h2>
-          <div className="indicates-required">
-            <span className="asterisk">*</span> indicates required
-          </div>
-          <div className="mc-field-group">
-            <label htmlFor="mce-FNAME">Nome </label>
-            <input
-              onChange={(e) => setData({ ...data, name: e.target.value })}
-              type="text"
-              value={name}
-              name="FNAME"
-              className=""
-              id="mce-FNAME"
-            />
-          </div>
-          <div className="mc-field-group">
-            <label htmlFor="mce-EMAIL">
-              E-mail <span className="asterisk">*</span>
-            </label>
-            <input
-              onChange={(e) => setData({ ...data, email: e.target.value })}
-              type="email"
-              value={email}
-              name="EMAIL"
-              className="required email"
-              id="mce-EMAIL"
-            />
-          </div>
-          <div className="mc-field-group">
-            <label htmlFor="mce-PHONE">Celular </label>
-            <input
-              onChange={(e) => setData({ ...data, phone: e.target.value })}
-              type="text"
-              name="PHONE"
-              className=""
-              value={phone}
-              id="mce-PHONE"
-            />
-          </div>
+        {/* <div id="mc_embed_signup_scroll"> */}
+        {/* <div className="indicates-required">
+              <span className="asterisk">*</span> indicates required
+            </div> */}
+        {/* <div className="mc-field-group"> */}
+        {/* <label htmlFor="mce-FNAME">Nome </label> */}
+        <input
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+          type="text"
+          value={name}
+          name="FNAME"
+          placeholder="Nome completo"
+          className=""
+          id="mce-FNAME"
+          required
+        />
+        {/* </div> */}
+        {/* <div className="mc-field-group"> */}
+        {/* <label htmlFor="mce-EMAIL">
+                E-mail <span className="asterisk">*</span>
+              </label> */}
+        <input
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+          type="email"
+          value={email}
+          name="EMAIL"
+          placeholder="Email"
+          className="required email"
+          id="mce-EMAIL"
+          required
+        />
+        {/* </div> */}
+        {/* <div className="mc-field-group"> */}
+        {/* <label htmlFor="mce-PHONE">Celular </label> */}
+        {/* <input
+            onChange={(e) => setData({ ...data, phone: e.target.value })}
+            type="text"
+            name="PHONE"
+            placeholder="Telefone"
+            className=""
+            value={phone}
+            id="mce-PHONE"
+          /> */}
+        <InputMask
+          name="PHONE"
+          id="mce-PHONE"
+          onChange={(e) => setData({ ...data, phone: e.target.value })}
+          placeholder="Telefone"
+          value={phone}
+          required
+          mask="+55\ 99 9999 99999"
+        />
+        {/* </div> */}
 
-          <div className="mc-field-group">
-            <label htmlFor="mce-EMAIL">
-              Voce trabalha com encapasulado?{" "}
-              <span className="asterisk">*</span>
-            </label>
-            <input
-              type="text"
-              // value=""
-              name="FNAME"
-              className=""
-              id="mce-FNAME"
-            />
-          </div>
+        {/* <div className="mc-field-group"> */}
+        {/* <label htmlFor="mce-EMAIL">
+                Voce trabalha com encapasulado?{" "}
+                <span className="asterisk">*</span> */}
+        {/* </label> */}
+        {/* <input
+                type="text"
+                // value=""
+                name="FNAME"
+                className=""
+                id="mce-FNAME"
+              /> */}
+        <span>Você já trabalha no mercado de encapsulados?</span>
+        <select
+          required
+          value={visited}
+          onChange={(e) => setData({ ...data, visited: e.target.value })}
+        >
+          <option value="Sim">Sim</option>
+          <option value="Não">Não</option>
+        </select>
+        {/* </div> */}
 
-          <div id="mce-responses" className="clear foot">
-            <div
-              className="response"
-              id="mce-error-response"
-              style={{ display: "none" }}
-            ></div>
-            <div
-              className="response"
-              id="mce-success-response"
-              style={{ display: "none" }}
-            ></div>
-          </div>
+        <div id="mce-responses" className="clear foot">
           <div
-            style={{ position: "absolute", left: "-5000px" }}
-            aria-hidden="true"
-          >
-            <input
-              type="text"
-              name="b_ed5da9de447f44d3ebe2fcaf6_b1c7d7f33e"
-              value=""
-            />
-          </div>
-          <div className="optionalParent">
-            <div className="clear foot">
-              <input
-                type="submit"
-                value="Enviar"
-                name="subscribe"
-                id="mc-embedded-subscribe"
-                className="button"
-                onClick={(e) => handleSubmit(e)}
-              />
-
-              {message && <span className="form-status">{message}</span>}
-            </div>
-          </div>
+            className="response"
+            id="mce-error-response"
+            style={{ display: "none" }}
+          ></div>
+          <div
+            className="response"
+            id="mce-success-response"
+            style={{ display: "none" }}
+          ></div>
         </div>
+        <div
+          style={{ position: "absolute", left: "-5000px" }}
+          aria-hidden="true"
+        >
+          <input
+            type="text"
+            name="b_ed5da9de447f44d3ebe2fcaf6_b1c7d7f33e"
+            value=""
+          />
+        </div>
+        {/* <div className="optionalParent">
+          <div className="clear foot"> */}
+        {/* <input
+              type="submit"
+              value="Enviar"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              className="button"
+              onClick={(e) => handleSubmit(e)}
+            /> */}
+
+        <button
+          disabled={loading}
+          type="submit"
+          value="Enviar"
+          name="subscribe"
+          id="mc-embedded-subscribe"
+          onClick={(e) => handleSubmit(e)}
+        >
+          {loading ? "ENVIANDO..." : "ENVIAR"}
+        </button>
+
+        {message && <span className="form-status">{message}</span>}
+        {/* </div>
+        </div> */}
+        {/* </div> */}
       </form>
-    </div>
+      {/* // </div> */}
+    </Styles.Container>
   );
 };
---
+++ src/components/Form/components/FormComplete/NewsLetterForm/styles.ts
@@ -0,0 +1,55 @@
+import styled, { css } from "styled-components";
+import media from "styled-media-query";
+import { Container as Wrapper } from "../../../../Container";
+
+export const Container = styled(Wrapper)`
+  ${({ theme }) => css`
+    display: flex;
+    background: rgba(255, 255, 255, 0.9);
+    max-width: 90%;
+    border-radius: 5px;
+
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    padding: ${theme.spacings.small};
+
+    h4 {
+      margin-bottom: ${theme.spacings.small};
+      font-size: ${theme.font.sizes.xxlarge};
+    }
+
+    ${media.lessThan("medium")`
+      max-width:none;
+    `}
+
+    form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
+
+      input,
+      select {
+        width: 100%;
+        padding: ${theme.spacings.xxsmall} ${theme.spacings.small};
+        margin-bottom: ${theme.spacings.xsmall};
+      }
+
+      span {
+        font-size: ${theme.font.sizes.small};
+        margin-bottom: ${theme.spacings.xsmall};
+      }
+
+      button {
+        width: 100%;
+        border-radius: 5px;
+        padding: ${theme.spacings.xsmall};
+        background: ${theme.colors.primary};
+        color: ${theme.colors.white};
+        font-weight: bold;
+      }
+    }
+  `}
+`;
