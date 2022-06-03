679f04cb24e3ae8b247a5e098c352b116134e3bc
Carlos Augusto <auggustus6@gmail.com>
2022-06-03 14:18:25 -0300

    fix: change scripts

--- src/pages/cadastro-sucesso.tsx
+++ src/pages/cadastro-sucesso.tsx
@@ -2,10 +2,31 @@ import React from "react";
 import { useTheme } from "styled-components";
 import Layout from "components/Layout";
 import ConsultantSuccessTemplate from "templates/ConsultantSuccessTemplate";
+import Head from "next/head";
 
 const ConsultantSuccess: React.FC = () => {
   const theme = useTheme();
-  return <ConsultantSuccessTemplate />;
+  return (
+    <>
+      <Head>
+        <script>
+          {`
+              !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '842527126718624');
+                fbq('track', 'Cadastro');
+              `}
+        </script>
+      </Head>
+      <ConsultantSuccessTemplate />;
+    </>
+  );
 };
 
 export default ConsultantSuccess;
